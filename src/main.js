0868e7ccb84c5d569e5863b12b66dee6a8622e99
Bryan Xing <bxing@my.bcit.ca>
2020-05-08 03:00:20 -0700

    initial task list

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
-import App from './home.svelte';
+import App from './app.svelte';
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'Team 23'
-	}
-});
-const app2 = new App({
-	target:document.body,
-	prosp: {
-		name: 'Team 23'
-	}
 });
 
 export default app;
