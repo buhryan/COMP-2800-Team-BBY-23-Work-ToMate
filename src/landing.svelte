c30cef15d5a86204586ef965080c61cf4a8a3b9e
Calvin Loungsay <Calvin.Loungsay@gmail.com>
2020-05-08 17:47:57 -0700

    Updated styling on landing

--- src/landing.svelte
+++ src/landing.svelte
@@ -1,2 +1,64 @@
+<style>
+  @media (min-width: 1025px) {
+    h1 {
+      color: #ee8152;
+      text-align:center;
+    }
+    button {
+      background-color: #ee8152;
+      color: #f7eac5;
+    }
+    div {
+      margin-left: 5%;
+      margin-right: 5%;
+      height: 30vh;
+      background: #f7eac5;
+    }
+  }
+  @media (min-width: 401px) and (max-width: 1024px) {
+    h1 {
+      color: #ee8152;
+      float:middle;
+    }
+    button {
+      background-color: #ee8152;
+      color: #f7eac5;
+    }
+    div {
+      margin-left: 5%;
+      margin-right: 5%;
+      height: 35vh;
+      background: #f7eac5;
+    }
+  }
+  @media (max-width: 400px) {
+    h1 {
+      color: #ee8152;
+      float:middle;
+    }
+    button {
+      background-color: #ee8152;
+      color: #f7eac5;
+    }
+    div {
+      margin-left: 5%;
+      margin-right: 5%;
+      height: 40vh;
+      background: #f7eac5;
+    }
+  }
+</style>
+
 <h1>Landing Page</h1>
-<a href="/home"><button>Login</button></a>
+<a href="/home">
+  <button>Login</button>
+</a>
+<button>SignUp</button>
+<div class="Square shape">
+  <h3>Core Features</h3>
+
+</div>
+<div class="Square shape">
+  <h3>About Us</h3>
+
+</div>
