09711e49db030a59ebcda4f010de365fd4056353
[Jessica Chu] <jessicachu81@gmail.com>
2020-05-14 11:38:55 -0700

    landing updated

--- src/landing.svelte
+++ src/landing.svelte
@@ -1,14 +1,27 @@
 <script>
-    import {db} from "./firebase.js";
+    import {
+        db
+    } from "./firebase.js";
     firebase.auth().onAuthStateChanged(function (user) {
-                    console.log(user.uid);
-                    if (user) {
-                        document.getElementById("loggedIn").style.display = "inline-block";
-                        document.getElementById("loginBtn").style.display = "none";
-                        document.getElementById("signupBtn").style.display = "none";
-                        
-                    }
-                })
+        console.log(user.uid);
+        if (user) {
+            document.getElementById("loggedIn").style.display = "inline";
+            document.getElementById("logout").style.display = "inline";
+            document.getElementById("loginBtn").style.display = "none";
+            document.getElementById("signupBtn").style.display = "none";
+
+        }
+    })
+
+    function logout() {
+        firebase.auth().signOut().then(() => {
+            document.getElementById("loginBtn").style.display = "inline";
+            document.getElementById("signupBtn").style.display = "inline";
+            document.getElementById("loggedIn").style.display = "none";
+            document.getElementById("logout").style.display = "none";
+        });
+
+    }
 </script>
 
 <style>
@@ -26,18 +39,27 @@
         width: 300px;
         background: #f7eac5;
     }
+
     #loggedIn {
         background-color: #ee8152;
         color: #f7eac5;
         width: 10px;
-        display:none;
+        display: none;
+    }
+
+    #logout {
+        display: none;
+        background-color: #ee8152;
+        color: #f7eac5;
+        width: 10px;
     }
 </style>
 
 <h1>Landing Page</h1>
 <span id="loginBtn"><a href="/login"><button>Login</button></a></span>
 <span id="signupBtn"><a href="/signup"><button>Signup</button></a></span>
-<span id = "loggedIn"><a href="/home"><button>Home</button></a></span>
+<span id="loggedIn"><a href="/home"><button>Home</button></a></span>
+<span id="logout"><button on:click={logout}>Logout</button></span>
 
 
 <div class="Square shape">
